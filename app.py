0a45ef41c19a118d9502cf58066f291dd7c53c89
Edgar Daniel <68125907+acostaedg@users.noreply.github.com>
2024-10-28 11:15:52 +0000

    add changes from replit

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return '¡Hola mundo desde Flask en Replit!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
